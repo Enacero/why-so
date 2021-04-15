31416290d44fae1abdbd6a5584d3eafdaa2aa2fc
Oleksii Petrenko <opetrenko@griddynamics.com>
2021-04-15 12:36:39 +0300

    b

--- controller/controller.py
+++ controller/controller.py
@@ -1,4 +1,4 @@
-from ryu.base import app_manager
+from ryu.app.simple_switch_14 import SimpleSwitch14
 from ryu.ofproto import ofproto_v1_4
 from ryu.controller.handler import set_ev_cls, MAIN_DISPATCHER
 from ryu.controller import ofp_event
@@ -7,25 +7,13 @@ import networkx as nx
 from matplotlib import pyplot as plt
 
 
-class Controller(app_manager.RyuApp):
+class Controller(SimpleSwitch14):
     OFP_VERSIONS = [ofproto_v1_4.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.graph = nx.Graph()
         self.id_counter = 1
-
-    @staticmethod
-    def add_flow(datapath, priority, match, actions):
-        ofproto = datapath.ofproto
-
-        parser = datapath.ofproto_parser
-
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
-        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                match=match, instructions=inst)
-        datapath.send_msg(mod)
 
     @set_ev_cls(topo_event.EventSwitchEnter)
     def new_switch(self, ev: topo_event.EventSwitchEnter):
